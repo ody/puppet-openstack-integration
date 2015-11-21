b67ac8a1fdd78c29ebedae7705dd7123b6eba537
Cody Herriges <cody@herriges.org>
2015-11-20 16:04:11 -0800

    (WIP) Add support for different module paths
      This commit adds the ability to read in from an environment variable
      the base path for where modules will be deployed, using the BASE_PATH
      variable.
    
      This needs to be done to support modern paths that are now default in
      Puppet 4.x packages delivered by Puppet Labs.  Specifically the use of
      /etc/puppetlabs/code in Puppet Enterprise 2015.2[1] and beyond plus the
      new PC1[2] compilation now used to delivery Open Source Puppet Agents.
    
      [1] http://docs.puppetlabs.com/pe/2015.2/index.html
      [2] https://docs.puppetlabs.com/puppet/4.3/reference/whered_it_go.html
    
    Change-Id: I2eddeaced8d2cb1467e396ba938698583d93f355

--- install_modules.sh
+++ install_modules.sh
@@ -6,11 +6,21 @@ if [ ! -z ${GEM_HOME} ]; then
     GEM_BIN_DIR=${GEM_HOME}/bin/
 fi
 
+if [ -z ${BASE_PATH} ]; then
+    echo 'BASE_PATH not set, assuming legacy paths'
+    BASE_PATH='/etc/puppet'
+fi
+
 export SCRIPT_DIR=$(cd `dirname $0` && pwd -P)
-export PUPPETFILE_DIR=${PUPPETFILE_DIR:-/etc/puppet/modules}
+export PUPPETFILE_DIR=${PUPPETFILE_DIR:-${BASE_PATH}/modules}
 source $SCRIPT_DIR/functions
 
 gem install r10k --no-ri --no-rdoc
+
+# There seems like no way to add privatebindir to path from beaker
+if [ ${BASE_PATH} = '/etc/puppetlabs/code' ]; then
+  ln -s /opt/puppetlabs/puppet/bin/r10k /opt/puppetlabs/bin/r10k
+fi
 
 # make sure there is no puppet module pre-installed
 rm -rf "${PUPPETFILE_DIR:?}/"*
@@ -18,3 +28,12 @@ rm -rf "${PUPPETFILE_DIR:?}/"*
 install_modules
 
 puppet module list
+
+# Hacks to prevent having to patch project-config; should be replaced with a
+# more intelligent jenkins/scripts/copy_puppet_logs.sh.
+if [ ${BASE_PATH} = '/etc/puppetlabs/code' ]; then
+  echo 'Deleting /etc/puppet so we can symlink it'
+  rm -rf /etc/puppet
+  echo 'Symlinked /etc/puppet to /etc/puppetlabs/code to get around static copy_puppet_logs.sh is project-config'
+  ln -s /etc/puppetlabs/code /etc/puppet
+fi
